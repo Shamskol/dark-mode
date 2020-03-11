c5102943afaa99635da4c474b9d7bce563aef72d
Kolade-Junaid <koladejun@yahoo.com>
2020-03-11 19:51:46 +0000

    Finished the initial project setup and created a hooks folder and a useLoclStorage.js inside the hooks folder

---
+++ build/precache-manifest.fa8799357649e91ebc86c2c593309c3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "961c40544dd4e194f2f23fd619b0759d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e1432345d355659a4d",
+    "url": "/static/css/main.2001245b.chunk.css"
+  },
+  {
+    "revision": "b31625d4330188644964",
+    "url": "/static/js/2.0daab0ed.chunk.js"
+  },
+  {
+    "revision": "896f0b8c06383492926956861e681256",
+    "url": "/static/js/2.0daab0ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e1432345d355659a4d",
+    "url": "/static/js/main.2aa7a263.chunk.js"
+  },
+  {
+    "revision": "11ec53e097561dfe6e55",
+    "url": "/static/js/runtime-main.cb63bbb1.js"
+  }
+]);
